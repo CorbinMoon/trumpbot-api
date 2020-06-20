3a0d3cf851de01c4c9928bbb2464833b8520b0f0
CorbinMoon <clangmoon@gmail.com>
2020-06-19 22:05:11 -0400

    refactor install_tensorflow.sh

--- bin/install_tensorflow.sh
+++ bin/install_tensorflow.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+tag="v1.15.0"
 wget https://github.com/bazelbuild/bazel/releases/download/0.24.1/bazel-0.24.1-installer-linux-x86_64.sh
 chmod +x bazel-0.24.1-installer-linux-x86_64.sh
 ./bazel-0.24.1-installer-linux-x86_64.sh
-git clone --branch v1.15.0 https://github.com/tensorflow/tensorflow --single-branch
+git clone --branch ${tag} https://github.com/tensorflow/tensorflow --single-branch
 chmod +x ./tensorflow/configure
-echo "y y y y y y y" | ./tensorflow/configure
+./tensorflow/configure
+bazel build --config=v1 //tensorflow/tools/pip_package:build_pip_package
+./bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
+pip install /tmp/tensorflow_pkg/tensorflow-version-tags.whl
