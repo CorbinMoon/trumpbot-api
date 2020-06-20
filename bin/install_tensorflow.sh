bbee12bae6bc3ba4c5d54fce2013a3cae28006ca
CorbinMoon <clangmoon@gmail.com>
2020-06-19 22:25:42 -0400

    added force yes

--- bin/install_tensorflow.sh
+++ bin/install_tensorflow.sh
@@ -4,8 +4,10 @@ wget https://github.com/bazelbuild/bazel/releases/download/0.24.1/bazel-0.24.1-i
 chmod +x bazel-0.24.1-installer-linux-x86_64.sh
 ./bazel-0.24.1-installer-linux-x86_64.sh
 git clone --branch ${tag} https://github.com/tensorflow/tensorflow --single-branch
-chmod +x ./tensorflow/configure
-./tensorflow/configure
+cd tensorflow
+chmod +x ./configure
+./configure
 bazel build --config=v1 //tensorflow/tools/pip_package:build_pip_package
 ./bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
 pip install /tmp/tensorflow_pkg/tensorflow-version-tags.whl
+cd ..
