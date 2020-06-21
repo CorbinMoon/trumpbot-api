853eb21eef296491daf1d3d8cdf77e5c6bb10599
CorbinMoon <clangmoon@gmail.com>
2020-06-20 20:27:03 -0400

    build tensorflow r1.15

--- bin/install_tensorflow.sh
+++ bin/install_tensorflow.sh
@@ -8,11 +8,12 @@ chmod +x bazel-3.1.0-installer-linux-x86_64.sh
 # get tensorflow sources
 git clone https://github.com/tensorflow/tensorflow.git
 cd tensorflow
+git checkout r1.15
 
 # build from source
 chmod +x ./configure
 ./configure
-bazel build --config=opt //tensorflow/tools/pip_package:build_pip_package
+bazel build --config=v1 //tensorflow/tools/pip_package:build_pip_package
 ./bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
 pip install /tmp/tensorflow_pkg/tensorflow-version-tags.whl
 cd ..
