2f2e179209f5c9114eeee136b8172883f7245b4a
CorbinMoon <clangmoon@gmail.com>
2020-06-20 20:54:22 -0400

    build tensorflow r1.14

--- bin/install_tensorflow.sh
+++ bin/install_tensorflow.sh
@@ -10,7 +10,7 @@ git checkout r1.14
 
 # build from source
 chmod +x ./configure && ./configure
-bazel build --config=v1 //tensorflow/tools/pip_package:build_pip_package
+bazel build //tensorflow/tools/pip_package:build_pip_package
 ./bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
 pip install /tmp/tensorflow_pkg/tensorflow-version-tags.whl
 cd ..
