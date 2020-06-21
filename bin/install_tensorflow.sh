0df5b1b70ba7d0f4c1af4802ef67cc9ec75c3dad
CorbinMoon <clangmoon@gmail.com>
2020-06-20 20:48:41 -0400

    build tensorflow r1.14

--- bin/install_tensorflow.sh
+++ bin/install_tensorflow.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
 #bazel installation
-wget https://github.com/bazelbuild/bazel/releases/download/3.1.0/bazel-3.1.0-installer-linux-x86_64.sh
-chmod +x bazel-3.1.0-installer-linux-x86_64.sh
-./bazel-3.1.0-installer-linux-x86_64.sh
+wget https://github.com/bazelbuild/bazel/releases/download/0.24.1/bazel-0.24.1-installer-linux-x86_64.sh
+chmod +x bazel-0.24.1-installer-linux-x86_64.sh && ./bazel-0.24.1-installer-linux-x86_64.sh
 
 # get tensorflow sources
-git clone https://github.com/tensorflow/tensorflow.git
-cd tensorflow
-git checkout r1.15
+git clone https://github.com/tensorflow/tensorflow.git && cd tensorflow
+git checkout r1.14
 
 # build from source
-chmod +x ./configure
-./configure
+chmod +x ./configure && ./configure
 bazel build --config=v1 //tensorflow/tools/pip_package:build_pip_package
 ./bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
 pip install /tmp/tensorflow_pkg/tensorflow-version-tags.whl
